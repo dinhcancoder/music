44343493e4b92aff7c1260e995133c38b2c3f01a
CaoDinhCan <89732863+CaoDinhCan@users.noreply.github.com>
2023-01-04 12:52:07 +0700

    new

--- js/main.js
+++ js/main.js
@@ -1,50 +1,50 @@
-let more = document.querySelector('.player-more ion-icon');
-more.addEventListener('click', () => {
-  more.classList.toggle('displayMore');
-})
+let more = document.querySelector(".player-more ion-icon");
+more.addEventListener("click", () => {
+  more.classList.toggle("displayMore");
+});
 
-let themeHeader = document.querySelector('.header-theme');
-let themeClose = document.querySelector('.theme-close');
-let theme = document.querySelector('.theme');
-let opacity = document.querySelector('.opacity');
+let themeHeader = document.querySelector(".header-theme");
+let themeClose = document.querySelector(".theme-close");
+let theme = document.querySelector(".theme");
+let opacity = document.querySelector(".opacity");
 
-themeHeader.addEventListener('click', () => {
-  theme.classList.toggle('displayTheme');
-  opacity.classList.toggle('displayTheme');
-})
+themeHeader.addEventListener("click", () => {
+  theme.classList.toggle("displayTheme");
+  opacity.classList.toggle("displayTheme");
+});
 
-themeClose.addEventListener('click', () => {
-  theme.classList.toggle('displayTheme');
-  opacity.classList.toggle('displayTheme');
-})
+themeClose.addEventListener("click", () => {
+  theme.classList.toggle("displayTheme");
+  opacity.classList.toggle("displayTheme");
+});
 
-opacity.addEventListener('click', () => {
-  opacity.classList.toggle('displayTheme');
-  theme.classList.toggle('displayTheme');
-})
+opacity.addEventListener("click", () => {
+  opacity.classList.toggle("displayTheme");
+  theme.classList.toggle("displayTheme");
+});
 
-let list = document.querySelector('.list');
-let playerListSong = document.querySelector('.player-listSong');
-playerListSong.addEventListener('click', () => {
-  playerListSong.classList.toggle('playerListSong');
-  list.classList.toggle('showlistMusic');
-})
+let list = document.querySelector(".list");
+let playerListSong = document.querySelector(".player-listSong");
+playerListSong.addEventListener("click", () => {
+  playerListSong.classList.toggle("playerListSong");
+  list.classList.toggle("showlistMusic");
+});
 
-let lyris = document.querySelector('.lyris');
-let lyrisClose = document.querySelector('.lyris-close');
-let lyrisSong = document.querySelector('.lyrisSong');
+let lyris = document.querySelector(".lyris");
+let lyrisClose = document.querySelector(".lyris-close");
+let lyrisSong = document.querySelector(".lyrisSong");
 let isLyris = false;
-lyrisSong.addEventListener('click', () => {
-  lyris.classList.toggle('showLyris');
+lyrisSong.addEventListener("click", () => {
+  lyris.classList.toggle("showLyris");
   if (isLyris) {
-    lyrisSong.style.color = ''
+    lyrisSong.style.color = "";
     isLyris = false;
   } else {
-    lyrisSong.style.color = '#c273ed'
+    lyrisSong.style.color = "#c273ed";
     isLyris = true;
   }
-})
+});
 
-lyrisClose.addEventListener('click', () => {
-  lyris.classList.toggle('showLyris');
-})
+lyrisClose.addEventListener("click", () => {
+  lyris.classList.toggle("showLyris");
+});
